5ad203809c1467bcdd4738765fc79a3567b2785e
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2024-06-25 16:34:49 -0700

    [SOCI-1] created project

---
+++ Socialcademy/Socialcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Socialcademy
+//
+//  Created by Jonathan Bigbee on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Jonathan Bigbee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
