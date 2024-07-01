2ca2ed42220bf5f1db8e285cf208fe4cab35d9a3
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2024-07-01 04:36:09 -0700

    [SOCI-32] handled model changes

--- Socialcademy/Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Socialcademy/Repositories/PostsRepository.swift
@@ -13,10 +13,12 @@ protocol PostsRepositoryProtocol {
     func fetchPosts() async throws -> [Post]
     func create(_ post: Post) async throws
     func delete(_ post: Post) async throws
+    func favorite(_ post: Post) async throws
+    func unfavorite(_ post: Post) async throws
 }
 
 struct PostsRepository: PostsRepositoryProtocol {
-    let postsReference = Firestore.firestore().collection("posts")
+    let postsReference = Firestore.firestore().collection("posts_v1")
     
     func create(_ post: Post) async throws {
         let document = postsReference.document(post.id.uuidString)
@@ -36,7 +38,16 @@ struct PostsRepository: PostsRepositoryProtocol {
         let document = postsReference.document(post.id.uuidString)
         try await document.delete()
     }
-
+    
+    func favorite(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.setData(["isFavorite": true], merge: true)
+    }
+    
+    func unfavorite(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.setData(["isFavorite": false], merge: true)
+    }
 }
 
 private extension DocumentReference {
@@ -55,6 +66,14 @@ private extension DocumentReference {
 
 #if DEBUG
 struct PostsRepositoryStub: PostsRepositoryProtocol {
+    func favorite(_ post: Post) async throws {
+    
+    }
+    
+    func unfavorite(_ post: Post) async throws {
+        
+    }
+    
     func delete(_ post: Post) async throws {
         
     }
