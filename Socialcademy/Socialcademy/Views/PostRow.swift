6a4f27b7f00e25d529a14bee45369333e4553c20
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2024-07-01 04:26:15 -0700

    [SOCI-30] handled errors

--- Socialcademy/Socialcademy/Views/PostRow.swift
+++ Socialcademy/Socialcademy/Views/PostRow.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct PostRow: View {
     @State private var showConfirmationDialog = false
+    @State private var error: Error?
 
     typealias DeleteAction = () async throws -> Void
     
@@ -45,14 +46,20 @@ struct PostRow: View {
         .confirmationDialog("Are you sure you want to delete this post?", isPresented: $showConfirmationDialog, titleVisibility: .visible) {
             Button("Delete", role: .destructive, action: deletePost)
         }
+        .alert("Cannot Delete Post", error: $error)
+
     }
     
     private func deletePost() {
         Task {
-            try! await deleteAction()
+            do {
+                try await deleteAction()
+            } catch {
+                print("[PostRow] Cannot delete post: \(error)")
+                self.error = error
+            }
         }
     }
-
 }
 
 #Preview {
