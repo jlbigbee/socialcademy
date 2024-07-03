cdd5e7888e0d1071cbeab835b9f0cac452750422
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2024-07-03 00:30:46 -0700

    [SOCI-48] updated the AuthView

--- Socialcademy/Socialcademy/View Models/AuthViewModel.swift
+++ Socialcademy/Socialcademy/View Models/AuthViewModel.swift
@@ -28,4 +28,26 @@ class AuthViewModel: ObservableObject {
             }
         }
     }
+    
+    func makeSignInViewModel() -> SignInViewModel {
+        return SignInViewModel(action: authService.signIn(email:password:))
+    }
+
+    func makeCreateAccountViewModel() -> CreateAccountViewModel {
+        return CreateAccountViewModel(action: authService.createAccount(name:email:password:))
+    }
+}
+
+extension AuthViewModel {
+    class SignInViewModel: FormViewModel<(email: String, password: String)> {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (email: "", password: ""), action: action)
+        }
+    }
+    
+    class CreateAccountViewModel: FormViewModel<(name: String, email: String, password: String)> {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (name: "", email: "", password: ""), action: action)
+        }
+    }
 }
--- Socialcademy/Socialcademy/Views/AuthView.swift
+++ Socialcademy/Socialcademy/Views/AuthView.swift
@@ -14,15 +14,47 @@ struct AuthView: View {
         if viewModel.isAuthenticated {
             MainTabView()
         } else {
-            Form {
-                TextField("Email", text: $viewModel.email)
-                SecureField("Password", text: $viewModel.password)
-                Button("Sign In", action: {
-                    viewModel.signIn()
-                })
+            NavigationView {
+                SignInForm(viewModel: viewModel.makeSignInViewModel()) {
+                    NavigationLink("Create Account", destination: CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel()))
+                }
             }
         }
     }
+}
+
+private extension AuthView{
+    struct SignInForm<Footer: View>: View {
+        @StateObject var viewModel: AuthViewModel.SignInViewModel
+        @ViewBuilder let footer: () -> Footer
+        
+        var body: some View {
+            Form {
+                TextField("Email", text: $viewModel.email)
+                SecureField("Password", text: $viewModel.password)
+                Button("Sign In", action: viewModel.submit)
+                footer()
+            }
+            .navigationTitle("Sign In")
+        }
+    }
+}
+
+private extension AuthView{
+    struct CreateAccountForm: View {
+        @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
+        
+        var body: some View {
+            Form {
+                TextField("Name", text: $viewModel.name)
+                TextField("Email", text: $viewModel.email)
+                SecureField("Password", text: $viewModel.password)
+                Button("Create Account", action: viewModel.submit)
+            }
+            .navigationTitle("Create Account")
+        }
+    }
+
 }
 
 #Preview {
